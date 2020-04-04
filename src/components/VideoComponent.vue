c20fe40e959640e27a0e471458ed1f633cd84c7c
evanawebb <webbevana@gmail.com>
2020-04-04 08:56:08 -0400

    add about text to video component

--- src/components/VideoComponent.vue
+++ src/components/VideoComponent.vue
@@ -1,5 +1,10 @@
 <template>
- <div class="container background-grey-light" style="padding-top: 10%;">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/PhhC_N6Bm_s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
- </div>
+  <div class="container background-grey-light">
+    <p style="padding-top: 5%; font-size: 2em;">
+      We are working on putting together a list of stories to share with you all, for now please enjoy the below compilation video to get a feel for the project.
+    </p>
+    <div  style="padding-top: 5%;">
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/PhhC_N6Bm_s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
 </template>
